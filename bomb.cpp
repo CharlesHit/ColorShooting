85b34d39428fddcf43cba37106e5de637c611cb3
Charles <Charles@Cha.local>
2019-11-04 14:21:55 -0500

    Charles_cleaning the code and add BOMB

---
+++ bomb.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Charles on 2019-11-04.
+//
+
+#include "bomb.h"
---
+++ bomb.h
@@ -0,0 +1,15 @@
+//
+// Created by Charles on 2019-11-04.
+//
+
+#ifndef TEAM16_BOMB_H
+#define TEAM16_BOMB_H
+
+
+class bomb
+{
+
+};
+
+
+#endif //TEAM16_BOMB_H
